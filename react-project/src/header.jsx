f2c8a2f9ca80a9b7ce26a97c69c2453c3f89b287
Sunil Salaria <sunilsalaria1993@gmail.com>
2025-07-30 21:44:11 +0530

    update and remove unwanted changes

---
+++ react-project/src/header.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Header(){
+    return(
+        <>
+        <h5>this is a header section</h5>
+        </>
+    )
+}
+
+export default Header
